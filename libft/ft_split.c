15a92cd4b15d0d75375255f213213147d1262c73
CanIBeFii <filipe250502@gmail.com>
2022-02-16 21:02:32 +0000

    Libft - ft_split

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -16,18 +16,59 @@ static int ft_split_counter(char *s, char c){
 static int	*ft_split_str_counter(char *s, char c, size_t delim){
 	int		*ret;
 	size_t	i;
+	size_t	e;
 	size_t	count;
 
-	i = 0;
+	i = -1;
+	count = 0;
+	e = 0;
 	ret = (int*)malloc(sizeof(int) * (delim - 1));
 	if(!ret)
 		return (ret);
-	while()
+	while(s[++i] != '\0'){
+		if(s[i] == c){
+			ret[e] = count;
+			e++;
+			count = 0;
+		}
+		else
+			count++;
+	}
+	return (ret);
+}
+
+static char *ft_the_spliter(char *s, size_t index, int *str_size){
+	size_t	i;
+	size_t	e;
+	char	*str;
+
+	i = -1;
+	while(++i < index)
+		e += str_size[i] + 1;
+	i = 0;
+	str = (char*)malloc(sizeof(char) * (str_size[index] + 1));
+	while(i < str_size[i]){
+		str[i] = s[e + i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
 
 char	**ft_split(char const *s, char c){
 	size_t	delim;
+	int		*str_size;
+	size_t	i;
+	char	**ret;
 
+	i = -1;
 	delim = ft_split_counter((char*)s, c);
-
+	str_size = ft_split_str_counter((char*)s, c, delim);
+	ret = (char**)malloc(sizeof(char*) * (delim));
+	if(!ret)
+		return (NULL);
+	while(++i < delim - 1)
+		ret[i] = ft_the_spliter((char*)s, i, str_size);
+	ret[i] = NULL;
+	return (ret);
 }
