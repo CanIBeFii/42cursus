223dfb7dbe6153650ab6dc643073597861763d4c
Filipe Mendes <filipe250502@gmail.com>
2022-03-03 17:12:25 +0000

    Gnl - Skeleton of some base fucntion
    Just makinf pseudo code to understand how to solve the problem

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,15 +6,30 @@
 /*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 16:39:23 by fialexan          #+#    #+#             */
-/*   Updated: 2022/03/03 16:46:31 by fialexan         ###   ########.fr       */
+/*   Updated: 2022/03/03 17:05:19 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_read(int fd)
+{
+	char	*str;
+	size_t	size_read;
+
+	str = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!str)
+		return (NULL);
+	size_read = read(fd, str, BUFFER_SIZE);
+	return (str);
+}
+
 char	*get_next_line(int fd)
 {
 	char	*tmp;
 
+	if (fd < 0 || BUFFER_SIZE < 1)
+		return (NULL);
+	tmp = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	tmp = read(fd, tmp, BUFFER_SIZE);
 }
