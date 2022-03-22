f1dee54ba5516621be85d388abbc118eab582e3b
Filipe Alexandre vidal mendes <fialexan@c1r15s4.42lisboa.com>
2022-03-22 09:56:38 +0000

    Gnl - added new funtion to .h file

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/03 16:26:24 by fialexan          #+#    #+#             */
-/*   Updated: 2022/03/04 09:29:14 by fialexan         ###   ########.fr       */
+/*   Updated: 2022/03/22 09:54:18 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,14 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-char	*get_next_line(int fd);
-size_t	ft_strlen(char	*str);
 char	*ft_read(int fd);
-char	*ft_strjoin(char *s1, char *s2);
+char	*ft_get_before_nl(char *str);
+char	*ft_get_after_nl(char *str);
+char	*ft_get_next_nl(char *str, int fd);
+char	*get_next_line(int fd);
+size_t	ft_strlen(char *str);
 size_t	ft_findchar(char *str, char c);
-char	*ft_strbreak(char *str, size_t size);
+char	*ft_strjoin(char *s1, char *s2);
+
 
 #endif
