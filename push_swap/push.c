3cbf8cad3d2f7cda79beae8d547b1d2e104862f3
Filipe Alexandre vidal mendes <fialexan@c1r15s4.42lisboa.com>
2022-05-19 15:07:24 +0100

    push_swap - All stack operations completed

--- push_swap/push.c
+++ push_swap/push.c
@@ -6,7 +6,7 @@
 /*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 14:49:26 by fialexan          #+#    #+#             */
-/*   Updated: 2022/05/19 14:57:51 by fialexan         ###   ########.fr       */
+/*   Updated: 2022/05/19 15:06:01 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,14 @@ void	push(t_stack **stack_sender, t_stack **stack_receiver)
 	(*stack_receiver) = (tmp);
 }
 
+void	pa(t_stack **stack_a, t_stack **stack_b)
+{
+	push(stack_b, stack_a);
+	ft_putendl_fd("pa", 1);
+}
 
+void	pb(t_stack **stack_a, t_stack **stack_b)
+{
+	push(stack_a, stack_b);
+	ft_putendl_fd("pb", 1);
+}
